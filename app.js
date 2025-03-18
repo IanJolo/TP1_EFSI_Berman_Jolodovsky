f4d357f988d9f4b5d8f0582ccae382e25856259b
Julian <48520440@est.ort.edu.ar>
2025-03-18 10:37:36 -0300

    creo que VF

--- app.js
+++ app.js
@@ -1,42 +1,100 @@
 let notaMate=document.getElementById("notaMate");
 let notaLengua=document.getElementById("notaLengua");
 let notaEfsi=document.getElementById("notaEfsi");
-
+let muestraResult=document.getElementById("muestraResult")
+let gif=document.getElementById("gifJava");
 
 document.getElementById("notaMate").addEventListener('input',  () => {
-    validarIngreso(notaMate);
+    cambiarInput(notaMate);
 })
 document.getElementById("notaLengua").addEventListener('input',  () => {
-    validarIngreso(notaLengua);
+    cambiarInput(notaLengua);
 })
 document.getElementById("notaEfsi").addEventListener('input',  () => {
-    validarIngreso(notaEfsi);
+    cambiarInput(notaEfsi);
 })
-
 function validarIngreso(nota){
     const notaMin=1;
     const notaMax=10;
-    let esValido=(nota.value>=notaMin && nota.value<=notaMax);
+    let esValido=(nota>=notaMin && nota<=notaMax);
+    return esValido;
+}
+function cambiarInput(nota){
+    let esValido=validarIngreso(nota.value);
     if(esValido){
-        if(nota.id="notaMate"){
-            notaMate.style.border="3px solid green";
-        }else if(nota.id="notaLengua"){
-            notaLengua.style.border="3px solid green";
-        }else (nota.id="notaEfsi");{
-            notaEfsi.style.border="3px solid green";
-        }
-    }else{
-        if(nota.id="notaMate"){
-            notaMate.style.border="3px solid red";
-        }else if(nota.id="notaLengua"){
-            notaLengua.style.border="3px solid red";
-        }else(nota.id="notaEfsi");{
-            notaEfsi.style.border="3px solid red";
-        }
+        nota.style.border="3px solid green"
+    }else {
+    nota.style.border="3px solid red"
+    }
+    if(nota.value==""){
+        nota.style.border="1px solid black";
     }
 }
+function validarNotas(){
+    let notasValidas=false;
+    if(validarIngreso(notaMate.value)==true && validarIngreso(notaLengua.value)==true && validarIngreso(notaEfsi.value)==true ){
+notasValidas=true;
+    }else {
+    notaMate.style.border="3px solid red";
+    notaEfsi.style.border="3px solid red";
+    notaLengua.style.border="3px solid red";
+    muestraResult.style.color="red";
+    muestraResult.innerHTML="Las notas ingresadas no son válidas";
+    }
+    return notasValidas;
+}
+    function calcularPromedio(){
+    const cantMaterias=3;
+    if(validarNotas()==true){
+    let suma = parseInt(notaEfsi.value) + parseInt(notaLengua.value) + parseInt(notaMate.value);
+    let promedio=suma/ cantMaterias;
+    if(promedio>=6){
+        if(promedio<8){
+        gif.setAttribute("src", "gifs/6.gif")
+        }else if(promedio<9){
+        gif.setAttribute("src", "gifs/8.gif")
+        }else {
+            gif.setAttribute("src", "gifs/10.gif")
+        }
+        muestraResult.style.color="green";
+    }else {
+        gif.setAttribute("src", "gifs/2.gif")
+        muestraResult.style.color="red";
+    }
+    muestraResult.innerHTML=`El promedio de notas es: ${promedio}`;
+}
+}
 
-
-calcularPromedio(){
-
+function calcularMateriaMax(){
+    let notas=[parseInt(notaMate.value), parseInt(notaLengua.value), parseInt(notaEfsi.value)]
+    let notaMax=0;
+    let materias=["Mate", "Lengua", "Efsi"];
+    let materiaMax="";
+    let listaMateriaMax=[];
+    let listaNotaMax=[];
+    if(validarNotas()==true){
+    for(let i=0;i<notas.length;i++){
+        if(notas[i]>notaMax){
+         notaMax=notas[i];
+         materiaMax=materias[i];
+        }else if(notas[i]===notaMax){
+            listaNotaMax.push(notas[i]);
+            listaMateriaMax.push(materias[i])
+        }
+    }
+    if(notaMax>listaNotaMax[0]){
+        listaNotaMax.splice(0, listaNotaMax.length)
+    }
+    muestraResult.style.color="blue";
+    if(listaNotaMax[0]==null){
+        gif.setAttribute("src", "gifs/6.gif")
+        muestraResult.innerHTML=`La materia con la mayor nota es: ${materiaMax}`;
+    }else if(listaNotaMax[1]==null){
+        gif.setAttribute("src", "gifs/8.gif")
+        muestraResult.innerHTML=`Las materias con la mayor nota es: ${materiaMax} y ${listaMateriaMax[0]}`;
+    }else {
+        gif.setAttribute("src", "gifs/10.gif")
+        muestraResult.innerHTML=`Las materias con la mayor nota es: ${materiaMax}, ${listaMateriaMax[0]} y ${listaMateriaMax[1]}`;
+    }
+}
 }
